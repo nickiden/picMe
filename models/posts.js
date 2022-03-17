74d14aa097462be124eb370cef91c27b9b246e26
Nick Iden <nickiden@Nicks-MacBook-Pro.local>
2022-03-16 23:35:04 -0400

    Started building out models for posts and comments. Also started looking into bcryptjs for user token

---
+++ models/posts.js
@@ -0,0 +1,26 @@
+// File :   posts.js
+// Comment  :
+//              model for posts created by user. Based on tutorial https://dev.to/tylerasa/learn-the-mern-stack-by-building-an-instagram-clone-part-one-188h
+
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const postSchema = new Schema({
+  caption: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+  username: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Post = mongoose.model("Post", postSchema);
+module.exports = Post;
